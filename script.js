4c1b08449bf449a3ddb3242ad1727c910a28fce2
Juan Caceres <manue1984.jc@gmail.com>
2025-03-04 13:59:30 +0100

    cambios index

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("El script está conectado correctamente.");
 
-
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
   let slides = document.querySelectorAll(".slide");
   let index = 0;
 
@@ -13,4 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(changeSlide, 3000); // Cambia cada 3 segundos
 });
+ 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuToggle.addEventListener("click", function () {
+      mobileMenu.classList.toggle("active");
+  });
+});
